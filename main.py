1ae1ca8805bb179746a479fd222b91d82c986faf
matt rhoads <mrhoads1@gmail.com>
2025-01-19 21:49:20 -0600

    adding player draw and update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     running = True
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +18,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
